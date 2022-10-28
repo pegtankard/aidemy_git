4bc84ce2ddae83fec083830df4e0be5fa060c9cc
fujimori <fujimori@fujimori-no-iMac-9.local>
2022-10-28 16:12:35 +0900

    revise wake to waka on line 9

--- show.py
+++ show.py
@@ -4,6 +4,6 @@
 import random
 
 with open("./hyakunin.txt", encoding="utf-8") as f:
-	wakes = [s.strip() for s in f.readlines()]
+	wakas = [s.strip() for s in f.readlines()]
 
 print(wakas[random.randrange(len(wakes))])
